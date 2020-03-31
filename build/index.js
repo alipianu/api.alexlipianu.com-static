3d5483762c935032278ef31a3bbeed8f919a27b1
alipianu <alexlipianu@live.ca>
2020-03-30 22:49:19 -0400

    auto-deployement

--- build/index.js
+++ build/index.js
@@ -24,5 +24,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //   .listen(config.core.service.port, NOTFOUND, () => console.log(`Listening on port ${config.core.service.port}..`));
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) { return res.send('Hello World!'); });
+app.get('/static', function (req, res) { return res.send('Hello World!'); });
 app.listen(3002, function () { return console.log("Example app listening on port " + 3002 + "!"); });
