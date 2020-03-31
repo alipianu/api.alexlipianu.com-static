e4f8a604bf2268e960c192d1eb49535b1ae40a1d
alipianu <alexlipianu@live.ca>
2020-03-30 22:15:11 -0400

    auto-deployement

---
+++ build/index.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var server_1 = require("../../../core/server");
+var config_json_1 = __importDefault(require("./config/config.json"));
+var errors_json_1 = __importDefault(require("./config/errors.json"));
+var endpoint_1 = require("../../../core/endpoint");
+var Content_1 = __importDefault(require("./controllers/Content"));
+var path_1 = require("path");
+// start api
+(new server_1.Server(config_json_1.default.service.id, errors_json_1.default, config_json_1.default.core.service.corsOptions))
+    .mountStaticRoutes({
+    '/images': path_1.join(__dirname, 'images')
+})
+    .mountRoutes({
+    '/content': {
+        get: {
+            '/:contentID/version/:clientVersion': Content_1.default.getContent,
+            '/test': Content_1.default.test
+        },
+        post: {
+            '/:contentID': Content_1.default.setContent
+        }
+    }
+})
+    .listen(config_json_1.default.core.service.port, endpoint_1.NOTFOUND, function () { return console.log("Listening on port " + config_json_1.default.core.service.port + ".."); });
