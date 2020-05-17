0db605e651a090d921627d8bdb26242666b07f88
alipianu <alexlipianu@live.ca>
2020-05-17 15:09:19 -0400

    auto-deployement

--- build/models/Content.js
+++ build/models/Content.js
@@ -111,7 +111,10 @@ exports.ContentSchema.statics.updateTarget = function (contentID, minClientVersi
             update = { $set: {} };
             update['$set']["data." + path] = items;
             console.log('update: ', JSON.stringify(update));
-            this.update({ contentID: contentID, minClientVersion: { $gte: minClientVersion }, maxClientVersion: { $lte: maxClientVersion } }, update);
+            this.update({ contentID: contentID, minClientVersion: { $gte: minClientVersion }, maxClientVersion: { $lte: maxClientVersion } }, update, function (err) {
+                if (err)
+                    console.log(err);
+            });
             return [2 /*return*/];
         });
     });
