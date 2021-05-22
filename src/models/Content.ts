df481cdb5c5d5af486a8068855e3545da46e997c
Alex Lipianu <social@alexlipianu.com>
2021-05-22 16:22:03 -0400

    Prep for auto-deploy (#5)

--- src/models/Content.ts
+++ src/models/Content.ts
@@ -1,5 +1,5 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
-import { __ERROR__ } from '../../../../../core/endpoint';
+import { __ERROR__ } from '@alipianu/microservice-core';
 import patterns from '../config/patterns.json';
 import config from '../config/config.json';
 mongoose.connect(`${config.service.database.url}/${config.service.database.name}`, { useNewUrlParser: true });
@@ -52,7 +52,7 @@ ContentSchema.statics.getContent = async function (contentID: string, clientVers
   if (!contentID.match(patterns.integer) || !clientVersion.match(patterns.integer)) throw __ERROR__("0100", "BADREQUEST");
   const id: number = parseInt(contentID);
   const version: number = parseInt(clientVersion);
-  let content = await this.findOne({ contentID: id, minClientVersion : { $lte : version }, maxClientVersion : { $gte : version } }, ['data'], { sort: { '_id': -1 } });
+  let content = await this.findOne({ contentID: id, minClientVersion: { $lte: version }, maxClientVersion: { $gte: version } }, ['data'], { sort: { '_id': -1 } });
   if (!content) throw __ERROR__("0100", "BADREQUEST");
   return content;
 };
@@ -69,11 +69,11 @@ ContentSchema.statics.getContent = async function (contentID: string, clientVers
 ContentSchema.statics.updateTarget = async function (contentID: number, minClientVersion: number, maxClientVersion: number, path: string, items: Array<any>) {
   // check for valid path
   if (!path.match(patterns.path)) return;
-  
+
   // update target content
   const update: any = { $set: {} };
   update['$set'][`data.${path}`] = items;
-  this.updateMany({ contentID, minClientVersion : { $gte : minClientVersion }, maxClientVersion : { $lte : maxClientVersion } }, update);
+  this.updateMany({ contentID, minClientVersion: { $gte: minClientVersion }, maxClientVersion: { $lte: maxClientVersion } }, update);
 };
 
 
