95a1319b35a7a5512e6f9c2fb9db2ed2f93a1fe3
Alex Lipianu <social@alexlipianu.com>
2021-05-23 19:18:25 -0400

    Added deployment script (#9)

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+readonly repo_path=$(git rev-parse --show-toplevel)
+if [ $? -ne 0 ]; then
+  echo "ERROR: Unable to get repo path." >&2
+  exit 1
+fi
+
+# navigate to repo root
+cd "$repo_path"
+
+# navigate up one directory into environment repo
+cd ..
+
+# get the environment path
+readonly env_path=$(git rev-parse --show-toplevel)
+if [ $? -ne 0 ]; then
+  echo "ERROR: Unable to get environment path." >&2
+  exit 2
+fi
+
+# navigate back to repo root
+cd "$repo_path"
+
+# run pm2 deploy
+"$env_path/scripts/pm2-deploy.sh"
+exit $?
